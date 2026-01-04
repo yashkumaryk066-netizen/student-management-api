43ea4649c805cd099c6877e40a384b138d005b8d
Student Management API <dev@studentapi.com>
2026-01-04 11:30:19 +0530

    FIX: Configure API_BASE_URL as relative path to fix connection issues

--- static/js/api.js
+++ static/js/api.js
@@ -1,6 +1,6 @@
 // API Helper Functions
 // API Helper Functions
-const API_BASE_URL = 'https://yashamishra.pythonanywhere.com/api';
+const API_BASE_URL = '/api';
 
 // Generic API call function
 async function apiCall(endpoint, options = {}) {
