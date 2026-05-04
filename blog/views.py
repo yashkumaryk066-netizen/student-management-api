0eb1a5ebb796f2b135d0fdb526cad1bc8a594524
Student Management API <dev@studentapi.com>
2026-05-04 10:56:34 +0530

    cleanup blog views

--- blog/views.py
+++ blog/views.py
@@ -2,23 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from django.http import HttpResponse
-
 def post_list(request):
+    object_list = Post.objects.filter(status='published')
+    paginator = Paginator(object_list, 9) # 9 posts per page
+    page = request.GET.get('page')
     try:
-        object_list = Post.objects.filter(status='published')
-        paginator = Paginator(object_list, 9) # 9 posts per page
-        page = request.GET.get('page')
-        try:
-            posts = paginator.page(page)
-        except PageNotAnInteger:
-            posts = paginator.page(1)
-        except EmptyPage:
-            posts = paginator.page(paginator.num_pages)
-        
-        return render(request, 'blog/list.html', {'posts': posts, 'page': page})
-    except Exception as e:
-        return HttpResponse(f"Error in Blog: {str(e)}", status=500)
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    
+    return render(request, 'blog/list.html', {'posts': posts, 'page': page})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, status='published')
